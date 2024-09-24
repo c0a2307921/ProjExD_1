263bd5658bf5825c2b741ba5de35291afbe32812
kou <c0a2307921@edu.teu.ac.jp>
2024-09-24 16:04:45 +0900

    ワープ解消

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,9 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.image.load("fig/pg_bg.jpg")
+    bg_img3 = pg.image.load("fig/pg_bg.jpg")
+    bg_img4 = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     tmr=0
@@ -17,8 +20,11 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -(tmr%800)
+        x = -(tmr%1600)
         screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img2, [x+1599, 0])
+        screen.blit(bg_img3, [x+3199, 0])
+        screen.blit(bg_img4, [x+4799, 0])
         screen.blit(kk_img, [300, 200]) 
         pg.display.update()
         tmr += 1        
