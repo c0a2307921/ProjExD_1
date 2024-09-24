b5fb5bc14368f9a36beed2efe32bc0713b80c706
kou <c0a2307921@edu.teu.ac.jp>
2024-09-24 16:38:06 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -16,19 +16,32 @@ def main():
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     tmr=0
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_list = pg.key.get_pressed()
+        if key_list[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        if key_list[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+        if key_list[pg.K_RIGHT]:
+           kk_rct.move_ip((2,0))
+        if key_list[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        kk_rct.move_ip((-1,0)) 
         x = -(tmr%1600)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2, [x+1599, 0])
         screen.blit(bg_img3, [x+3199, 0])
         screen.blit(bg_img4, [x+4799, 0])
-        screen.blit(kk_img, [300, 200]) 
+        screen.blit(kk_img,kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
+        key_list = pg.key.get_pressed()
 
 
 if __name__ == "__main__":
